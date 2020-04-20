5fcb201d69f8c317d73780af7f48d77033164c94
Kevin Howley <20078896@mail.wit.ie>
2020-04-20 15:25:23 +0100

    New Views again

---
+++ app/src/main/java/org/wit/marshalmate/activities/fragments/EditFragment.kt
@@ -0,0 +1,40 @@
+package org.wit.marshalmate.activities.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fr_search.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.wit.marshalmate.R
+import org.wit.marshalmate.activities.MainActivity
+import org.wit.marshalmate.activities.helpers.EventAdapter
+
+import org.wit.marshalmate.models.EventModel
+
+class EditFragment : Fragment() , AnkoLogger {
+    private var events=ArrayList<EventModel>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fr_edit, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity!!.title = "Edit"
+        events=(activity as MainActivity).getAllEvents()
+
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/org/wit/marshalmate/activities/fragments/HomeFragment.kt
+++ app/src/main/java/org/wit/marshalmate/activities/fragments/HomeFragment.kt
@@ -1,31 +1,27 @@
 package org.wit.marshalmate.activities.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fr_home_screen.*
-import kotlinx.android.synthetic.main.fr_search.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-
 import org.wit.marshalmate.R
 import org.wit.marshalmate.activities.MainActivity
 import org.wit.marshalmate.activities.helpers.EventAdapter
 import org.wit.marshalmate.activities.helpers.EventListener
-
 import org.wit.marshalmate.models.EventModel
 
-private var events=ArrayList<EventModel>()
 
+private var events=ArrayList<EventModel>()
 private var ownedEvents=ArrayList<EventModel>()
+private var eventPartOf=ArrayList<EventModel>()
+
 val user = FirebaseAuth.getInstance().currentUser
 val owner=user?.email.toString()
 
@@ -42,29 +38,66 @@ class HomeFragment : Fragment(),AnkoLogger,EventListener {
 
         //getting Owned Events only
         events=(activity as MainActivity).getAllEvents()
+        ownedEvents.clear()
         for (i in events){
             if(i.creator.equals(owner)){
-                ownedEvents.add(i)
+                ownedEvents.add(i.copy())
+            }
+        }
+        //getting events that the user is a part of
+        eventPartOf.clear()
+        for(i in events){
+            for(j in i.points){
+                if(j.assignedUser.equals(owner)){
+                    eventPartOf.add(i)
+                }
             }
         }
 
+        //duplicate remover
+        val tempOwned= ownedEvents
+        val tempPartOf= eventPartOf
 
-
-
+        //pass the events to recycler views
         val mEventAdapter=EventAdapter(ownedEvents,this)
         ownedEventsRecylcerView.layoutManager=LinearLayoutManager(context)
         ownedEventsRecylcerView.adapter=mEventAdapter
 
+        val nEventAdapter=EventAdapter(eventPartOf,this)
+        upcomingEventsRecyclerView.layoutManager=LinearLayoutManager(context)
+        upcomingEventsRecyclerView.adapter=nEventAdapter
+
+
+
+
+    }
+
+    fun removeDuplicates(){
 
     }
 
     override fun onEventClick(event: EventModel) {
         if (event.creator.equals(owner)){
             Toast.makeText(context,"This is an owned event", Toast.LENGTH_SHORT).show()
+            val t: FragmentTransaction = this.fragmentManager!!.beginTransaction()
+            val mFrag: Fragment = EditFragment()
+            t.replace(R.id.content_frame, mFrag)
+            t.commit()
+
         }
         else{
             Toast.makeText(context,"Not Owned", Toast.LENGTH_SHORT).show()
+
+            val t: FragmentTransaction = this.fragmentManager!!.beginTransaction()
+            val mFrag: Fragment = ViewFragment()
+            t.replace(R.id.content_frame, mFrag)
+            t.commit()
         }
 
     }
+
+
+
+
+
 }
